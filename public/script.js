3b6fdd3e4d0289ce0f495cb3494a1e7d6bcad922
bgoodhue8 <6305+bgoodhue8@users.noreply.github.com>
2021-03-05 17:44:26 -0500

    script

---
+++ public/script.js
@@ -0,0 +1,52 @@
+async function windowActions() {
+
+const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+const cities = [];
+
+//fetch(endpoint)
+const request = await fetch(endpoint)
+    .then(blob => blob.json())
+    .then(data => cities.push(...data))
+
+console.log(cities);
+
+function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.city.match(regex) || place.zip.match(regex)
+    })
+}
+
+function numberWithCommas(x) {
+return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+}
+
+function displayMatches(event) {
+const matchArray = findMatches(event.target.value, cities);
+const html = matchArray.map(place => {
+    const regex = new RegExp(this.value, 'gi')
+    const cityName = place.city.replace(regex, `<span class='h1'>${this.value}</span>`);
+    const stateName = place.state.replace(regex, `<span class='h1'>${this.value}</span>`);
+    return `
+    <li>
+        <span class="name">${cityName}, ${stateName}</span>
+        <span class="population">${numberWithCommas(place.population)}</span>
+    </li>
+`;
+}).join('');
+suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions')
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', (evt) => {
+  displayMatches(evt)
+});
+}
+
+if (typeof window !== "undefined"){
+  window.onload = windowActions;
+  const arrayName = await request.json()
+}
