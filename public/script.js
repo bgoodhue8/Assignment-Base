b5f4e2722505a3ae2c964b96318ae3cc94d788e0
bgoodhue8 <6305+bgoodhue8@users.noreply.github.com>
2021-03-08 18:12:38 -0500

    h

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ async function windowActions() {
 const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 const cities = [];
 
-//fetch(endpoint)
+fetch(endpoint)
 const request = await fetch(endpoint)
     .then(blob => blob.json())
     .then(data => cities.push(...data))
@@ -13,7 +13,7 @@ console.log(cities);
 function findMatches(wordToMatch, cities) {
     return cities.filter(place => {
         const regex = new RegExp(wordToMatch, 'gi');
-        return place.city.match(regex) || place.zip.match(regex)
+        return place.city.match(regex) || place.state.match(regex)
     })
 }
 
@@ -22,11 +22,11 @@ return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 }
 
 function displayMatches(event) {
-const matchArray = findMatches(event.target.value, cities);
-const html = matchArray.map(place => {
-    const regex = new RegExp(this.value, 'gi')
-    const cityName = place.city.replace(regex, `<span class='h1'>${this.value}</span>`);
-    const stateName = place.state.replace(regex, `<span class='h1'>${this.value}</span>`);
+    const matchArray = findMatches(event.target.value, cities);
+    const html = matchArray.map(place => {
+        const regex = new RegExp(event.target.value, 'gi')
+        const cityName = place.city.replace(regex, `<span class='h1'>${event.target.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class='h1'>${event.target.value}</span>`);
     return `
     <li>
         <span class="name">${cityName}, ${stateName}</span>
