467cd3f0ae61c2e9775a33bd215cb74ffc468c63
bgoodhue8 <6305+bgoodhue8@users.noreply.github.com>
2021-03-10 13:50:16 -0500

    final edits, this works now

--- public/script.js
+++ public/script.js
@@ -5,31 +5,33 @@ async function windowActions() {
     const searchInput = document.querySelector('.search');
     const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 
-const request = await fetch('/api')
-const data = await request.json();
+    const request = await fetch('/api')
+    const data = await request.json();
 
-function findMatches(wordToMatch, data) {
-    return cities.filter(place => {
-        const regex = new RegExp(wordToMatch, 'gi');
-        return place.match.match(regex) || place.zip.match(regex)
-        || place.category.match(regex) || place.zip.match(regex);
+    function findMatches(wordToMatch, data) {
+        return data.filter((place) => {
+            const regex = new RegExp(wordToMatch, 'gi');
+            return place.city.match(regex) || place.name.match(regex)
+            || place.category.match(regex) || place.zip.match(regex);
+        });
+    }
+
+    function displayMatches(event) {
+        const matchArray = findMatches(event.target.value, data);
+        const html = matchArray.map((place) =>  `
+        <li>
+            <span class="name">${place.name.toLowerCase()}</span>
+            <span class="category">${place.category.toLowerCase()}</span>
+            <span class="city">${place.city.toLowerCase()}</span>
+            <span class="zip">${place.zip.toLowerCase()}</span>
+        </li>
+    `).join('');
+    suggestions.innerHTML = html;
+    }
+
+    searchInput.addEventListener('keyup', (event) => {
+        event.preventDefault();
+        displayMatches(event);
     });
-}
-
-function displayMatches(event) {
-    const matchArray = findMatches(event.target.value, data);
-    const html = matchArray.map((place) => { `
-    <li>
-        <span class="name">${place.name.toLowerCase()}</span>
-        <span class="category">${place.category.toLowerCase()}</span>
-        <span class="city">${place.city.toLowerCase()}</span>
-        <span class="zip">${place.zip.toLowerCase()}</span>
-    </li>
-`).join('');
-suggestions.innerHTML = html;
-}
-
-searchInput.addEventListener('keyup', (event) => {
-    event.preventDefault();
-    displayMatches(event);
-});
+} 
+window.onload = windowActions;
